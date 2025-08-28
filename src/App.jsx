24f9cd4c39448235981d97de0484c9bfb0533e0b
Riyanto24434 <aris.riyanto24434@gmail.com>
2025-08-28 09:42:05 +0700

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl text-red-600 font-bold">Hello World</h1>
+      <h1 class="text-8xl text-blue-700 font-extrabold" >Helo Aris</h1>
+    
+    </>
+  );
+}
+
+export default App;
